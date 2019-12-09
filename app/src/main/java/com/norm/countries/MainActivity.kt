d2c7fc1b492150b01d53f7408cbd48b2f6e13e00
Zatt Wine <tomclick.org@gmail.com>
2019-12-09 16:08:14 +0630

    init project

---
+++ app/src/main/java/com/norm/countries/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.norm.countries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+}
